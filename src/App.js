3a47f318d7d78d57314fb9a2a063d8e0512d85ec
HyosonChoi <chsnizzi0@gmail.com>
2024-10-21 11:27:41 +0900

    redirect 설정

--- src/App.js
+++ src/App.js
@@ -9,17 +9,12 @@ import Header from "./components/Header";  // Header 컴포넌트 불러오기
 
 function App() {
   return (
-    <div style={{ position: "relative", height: "100vh", paddingTop: "60px" }}> {/* 상단 여백 추가 */}
-      {/* 헤더 컴포넌트를 상단에 추가 */}
-      <Header />
-
-      {/* 라우팅 설정 */}
       <Routes>
         <Route path="/" element={<TodoPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
-    </div>
+
   );
 }
 
