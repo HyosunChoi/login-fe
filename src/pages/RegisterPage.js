3e05b990dddfa503dfd041e81fce8fcc673c2889
HyosonChoi <chsnizzi0@gmail.com>
2024-10-23 02:23:04 +0900

    error fixed

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -15,44 +15,34 @@ const RegisterPage = () => {
   const handleSubmit = async (event) => {
     event.preventDefault(); // 페이지 새로고침 방지
 
-    // 필수값 검증
-    if (!name || !email || !password || !secPassword) {
-      setError("모든 필수값을 입력해 주세요.");
-      return; // 필수값이 없으면 요청을 보내지 않음
-    }
-
     try {
+
+      if (!name || !email || !password || !secPassword) {
+        throw new Error("모든 필수값을 입력해 주세요.");
+        
+      }
+  
       if (password !== secPassword) {
-        setError("패스워드가 일치하지 않습니다. 다시 입력해 주세요");
+        setError("패스워드가 일치하지 않습니다. 다시 입력해 주세요.");
         return;
       }
-
       const response = await api.post("/user", { name, email, password });
       if (response.status === 200) {
-        // 회원가입 성공 시 로그인 페이지로 이동
         navigate("/login");
-      } else {
-        throw new Error(response.data.error); // 일반 오류 처리
+
+      }else {
+        throw new Error(response.data.error);
       }
     } catch (error) {
-      // 에러 객체 콘솔에 출력하여 상태 확인
-      console.log("에러 객체 전체:", error);
-
-      // 백엔드에서 전달한 에러 메시지를 처리하여 화면에 출력
-      if (error.response && error.response.data && error.response.data.err) {
-        setError(error.response.data.err);  // "이미 가입된 유저입니다" 메시지 처리
-        console.log("에러 메시지:", error.response.data.err);
-      } else {
-        setError(error.message);  // 일반 오류 처리
-      }
+      setError(error.message);
     }
   };
 
   return (
     <div className="display-center">
-      {/* 에러가 있을 경우에만 에러 메시지 출력 */}
+      {/* 에러 메시지가 있을 경우 상단에 출력 */}
       {error && (
-        <div style={{ color: "red", marginBottom: "10px" }}>
+        <div style={{ color: "red", marginBottom: "20px" }}>
           {error}
         </div>
       )}
